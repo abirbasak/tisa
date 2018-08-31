45f345d5fc47aa93fd4e241eab4afb94b3f23817
abirbasak <abirbasak@gmail.com>
2018-08-31 15:30:39 +0530

    adding test cases for sequential

---
+++ test/azuik/algorithm/sequential_test.cpp
@@ -0,0 +1,34 @@
+
+#include <azuik/algorithm/sequential.hpp>
+#include <azuik/test/unit_test.hpp>
+#include <vector>
+AZUIK_TEST_SUIT(sequential)
+{
+    using namespace azuik;
+    AZUIK_TEST_CASE(all_of)
+    {
+        std::vector<int> v1{1, 2, 3, 4, 5};
+        std::vector<int> v2{2, 2, 2, 2, 2};
+        AZUIK_TEST(core::all_of(v1, 3) == false);
+        AZUIK_TEST(core::all_of(v2, 2) == true);
+        AZUIK_TEST(core::all_of(v2, 3) == false);
+    }
+    AZUIK_TEST_CASE(none_of)
+    {
+        std::vector<int> v1{1, 2, 3, 4, 5};
+        std::vector<int> v2{2, 2, 2, 2, 2};
+        AZUIK_TEST(core::none_of(v1, 3) == false);
+        AZUIK_TEST(core::none_of(v2, 20) == true);
+        AZUIK_TEST(core::none_of(v2, 3) == true);
+        AZUIK_TEST(core::none_of(v2, 2) == false);
+    }
+    AZUIK_TEST_CASE(any_of)
+    {
+        std::vector<int> v1{1, 2, 3, 4, 5};
+        std::vector<int> v2{2, 2, 2, 2, 2};
+        AZUIK_TEST(core::any_of(v1, 3) == true);
+        AZUIK_TEST(core::any_of(v2, 6) == false);
+        AZUIK_TEST(core::any_of(v2, 3) == false);
+        AZUIK_TEST(core::any_of(v2, 2) == true);
+    }
+}
