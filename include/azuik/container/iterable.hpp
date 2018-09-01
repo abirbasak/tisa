c7701c64fb860cf6351041a81a1fdbbb85f91c7a
abirbasak <abirbasak@gmail.com>
2018-09-01 23:20:24 +0530

    adding some standard iterator implementation

--- include/azuik/container/iterable.hpp
+++ include/azuik/container/iterable.hpp
@@ -16,42 +16,127 @@ namespace azuik
         template <class Fn>
         class iterable {};
 
-        template <class S>
-        class bidirectional_iterator {
-            template <class>
-            friend class bidirectional_iterator;
+        template <class S, class Policy>
+        class standard_iterator : private Policy {
+        private:
+            using base_type = Policy;
 
         public:
-            using self_type = bidirectional_iterator;
-            using const_self = bidirectional_iterator<add_const<S>>;
-            using nonconst_self = bidirectional_iterator<remove_const<S>>;
-            using value_type = value_type<S>;
-            using difference_type = difference_type<S>;
-            using reference = reference<S>;
-            using pointer = pointer<S>;
-            using iterator_category = bidirectional_tag;
+            using self_type = standard_iterator;
+            using other_type = standard_iterator<flip_const<S>, Policy>;
+            using value_type = core::value_type<S>;
+            using difference_type = core::difference_type<S>;
+            using reference = core::reference<S>;
+            using pointer = core::pointer<S>;
+            using iterator_category = typename base_type::iterator_category;
 
         private:
-            using node_ptr = typename S::node_ptr;
+            using nonconst_self = standard_iterator<remove_const<S>, Policy>;
 
-        private:
-            static void increment(self_type& self)
+        public:
+            explicit constexpr standard_iterator(nonconst_self that) noexcept
+                : base_type{that}
+            {}
+            constexpr self_type& operator=(nonconst_self& that) noexcept
             {
-                self.m_ptr = (*self.m_ptr).next;
+                static_cast<base_type&>(*this) = that;
+                return *this;
             }
-            static void decrement(self_type& self)
+            constexpr auto operator*() const noexcept
             {
-                self.m_ptr = (*self.m_ptr).prev;
+                return base_type::deref();
             }
-            static reference dereference(self_type& self)
+            constexpr auto operator++() noexcept
             {
-                return (*self.m_ptr).value;
+                base_type::next();
+                return *this;
             }
-
-        private:
-            node_ptr m_ptr;
+            constexpr auto operator--() noexcept
+            {
+                base_type::prev();
+                return *this;
+            }
+            constexpr auto operator++(int) noexcept
+            {
+                self_type temp{*this};
+                ++(*this);
+                return temp;
+            }
+            constexpr auto operator--(int) noexcept
+            {
+                self_type temp{*this};
+                --(*this);
+                return temp;
+            }
+            constexpr auto operator==(self_type const& that) const noexcept
+            {
+                return base_type::equal(that);
+            }
+            constexpr auto operator!=(self_type const& that) const noexcept
+            {
+                return !base_type::equal(that);
+            }
+            constexpr auto operator==(other_type const& that) const noexcept
+            {
+                return base_type::equal(that);
+            }
+            constexpr auto operator!=(other_type const& that) const noexcept
+            {
+                return !base_type::equal(that);
+            }
         };
 
+        namespace detail_
+        {
+            template <class S>
+            struct forward_policy {
+                using node_ptr = cond<is_const<S>, typename S::node_cptr, typename S::node_ptr>;
+                using iterator_category = std::forward_iterator_tag;
+
+                constexpr auto deref() const noexcept
+                {
+                    return *m_ptr;
+                }
+                constexpr auto increment() noexcept
+                {
+                    m_ptr = (*m_ptr).next;
+                }
+                template <class That>
+                constexpr auto equal(That const& that) const noexcept
+                {
+                    return m_ptr == that.m_ptr;
+                }
+                node_ptr m_ptr;
+            };
+            template <class S>
+            struct bidirectional_policy {
+                using node_ptr = cond<is_const<S>, typename S::node_cptr, typename S::node_ptr>;
+                using iterator_category = std::bidirectional_iterator_tag;
+
+                constexpr auto deref() const noexcept
+                {
+                    return *m_ptr;
+                }
+                constexpr auto increment() noexcept
+                {
+                    m_ptr = (*m_ptr).next;
+                }
+                constexpr auto decrement() noexcept
+                {
+                    m_ptr = (*m_ptr).prev;
+                }
+                template <class That>
+                constexpr auto equal(That const& that) const noexcept
+                {
+                    return m_ptr == that.m_ptr;
+                }
+                node_ptr m_ptr;
+            };
+        } // namespace detail_
+        template <class S>
+        using forward_iterator = standard_iterator<S, detail_::forward_policy<S>>;
+        template <class S>
+        using bidirectional_iterator = standard_iterator<S, detail_::bidirectional_policy<S>>;
     } // namespace core
 
 } // namespace azuik
