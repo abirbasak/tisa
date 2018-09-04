03fac31fe8600c7341e7fccb859a77102e7f8a0d
abirbasak <abirbasak@gmail.com>
2018-09-04 22:50:44 +0530

    adding more test cases for list & forward_list

--- test/azuik/container/list_test.cpp
+++ test/azuik/container/list_test.cpp
@@ -4,11 +4,86 @@ using namespace azuik;
 
 AZUIK_TEST_SUIT(list_test)
 {
-    AZUIK_TEST_CASE(empty)
+    AZUIK_TEST_CASE(empty1)
     {
         core::forward_list<int> l;
         AZUIK_TEST(l.empty() == true);
         l.push_front(1);
         AZUIK_TEST(l.empty() == false);
+    }
+    AZUIK_TEST_CASE(empty2)
+    {
+        core::list<int> l;
+        AZUIK_TEST(l.empty() == true);
+        l.push_front(1);
+        AZUIK_TEST(l.empty() == false);
+    }
+    AZUIK_TEST_CASE(count1)
+    {
+        core::forward_list<int> l;
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 0);
+        l.push_front(10);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 1);
+        l.push_front(15);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 2);
+        l.push_front(20);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 3);
+        l.push_front(25);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 4);
+
+        l.pop_front();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 3);
+        l.pop_front();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 2);
+        l.pop_front();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 1);
+        l.pop_front();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 0);
+    }
+    AZUIK_TEST_CASE(count2)
+    {
+        core::list<int> l;
+        l.begin();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 0);
+        l.push_front(10);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 1);
+        l.push_front(15);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 2);
+        l.push_front(20);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 3);
+        l.push_front(25);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 4);
+
+        l.pop_front();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 3);
+        l.pop_front();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 2);
+        l.pop_front();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 1);
+        l.pop_front();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 0);
+    }
+    AZUIK_TEST_CASE(count3)
+    {
+        core::list<int> l;
+        l.begin();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 0);
+        l.push_back(10);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 1);
+        l.push_back(15);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 2);
+        l.push_back(20);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 3);
+        l.push_back(25);
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 4);
+
+        l.pop_back();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 3);
+        l.pop_back();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 2);
+        l.pop_back();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 1);
+        l.pop_back();
+        AZUIK_TEST(std::distance(l.begin(), l.end()) == 0);
     }
 }
