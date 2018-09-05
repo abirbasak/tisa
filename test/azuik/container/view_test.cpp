ba7a330eb3e1c05c9bb0ff52487bac97765eefbb
abirbasak <abirbasak@gmail.com>
2018-09-05 14:43:20 +0530

    adding a few test cases for view

---
+++ test/azuik/container/view_test.cpp
@@ -0,0 +1,23 @@
+#include <azuik/container/view.hpp>
+#include <azuik/test/unit_test.hpp>
+#include <vector>
+AZUIK_TEST_SUIT(view)
+{
+    using namespace azuik;
+    AZUIK_TEST_CASE(empty)
+    {
+        using S = std::vector<int>;
+        S v{};
+        using RV = core::range_view<S>;
+        core::range_view<S> rv{v};
+        AZUIK_TEST(rv.empty() == true);
+    }
+    AZUIK_TEST_CASE(size)
+    {
+        using S = std::vector<int>;
+        S v{1, 3, 5, 7};
+        using RV = core::range_view<S>;
+        core::range_view<S> rv{v};
+        AZUIK_TEST(rv.size() == v.size());
+    }
+}
