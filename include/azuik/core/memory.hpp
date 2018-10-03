0d37c8f1d4be938caa1cd61927fb7bc9e393b7e9
abirbasak <abirbasak@gmail.com>
2018-10-03 15:53:55 +0530

    adding several new functions to memory

--- include/azuik/core/memory.hpp
+++ include/azuik/core/memory.hpp
@@ -2,6 +2,7 @@
 #define AZUIK_CORE_MEMORY_HPP
 
 #include <azuik/core/mpl.hpp>
+#include <memory>
 
 namespace azuik
 {
@@ -54,6 +55,117 @@ namespace azuik
                 }
             }
         } const destroy_range{};
+
+        struct uninitialized_fill_fn {
+            template <class InIter, class T>
+            void operator()(InIter first, InIter last, T const& x) const
+            {
+                return std::uninitialized_fill(first, last, x);
+            }
+        };
+        inline constexpr uninitialized_fill_fn uninitialized_fill = {};
+
+        struct uninitialized_fill_n_fn {
+            template <class InIter, class Size, class T>
+            auto operator()(InIter first, Size n, T const& x) const
+            {
+                return std::uninitialized_fill_n(first, n, x);
+            }
+            template <class InIter, class Size>
+            auto operator()(InIter first, Size n) const
+            {
+                using T = core::value_type<InIter>;
+                return std::uninitialized_fill_n(first, n, T{});
+            }
+        };
+        inline constexpr uninitialized_fill_n_fn uninitialized_fill_n = {};
+
+        struct uninitialized_value_construct_fn {
+            template <class InIter>
+            auto constexpr operator()(InIter first, InIter last) const -> void
+            {
+                return std::uninitialized_value_construct(first, last);
+            }
+        };
+        inline constexpr uninitialized_value_construct_fn uninitialized_value_construct = {};
+
+        struct uninitialized_value_construct_n_fn {
+            template <class FwdIter, class Size>
+            auto constexpr operator()(FwdIter first, Size n) const -> FwdIter
+            {
+                return std::uninitialized_value_construct_n(first, n);
+            }
+        };
+        inline constexpr uninitialized_value_construct_n_fn uninitialized_value_construct_n = {};
+
+        struct uninitialized_default_construct_fn {
+            template <class InIter>
+            auto constexpr operator()(InIter first, InIter last) const -> void
+            {
+                return std::uninitialized_default_construct(first, last);
+            }
+        };
+        inline constexpr uninitialized_default_construct_fn uninitialized_default_construct = {};
+
+        struct uninitialized_default_construct_n_fn {
+            template <class FwdIter, class Size>
+            auto constexpr operator()(FwdIter first, Size n) const -> FwdIter
+            {
+                return std::uninitialized_default_construct_n(first, n);
+            }
+        };
+        inline constexpr uninitialized_default_construct_n_fn uninitialized_default_construct_n =
+            {};
+
+        struct uninitialized_copy_fn {
+            template <class InIter, class FwdIter>
+            auto constexpr operator()(InIter first, InIter last, FwdIter result) const -> FwdIter
+            {
+                return std::uninitialized_copy(first, last, result);
+            }
+        };
+        inline constexpr uninitialized_copy_fn uninitialized_copy = {};
+
+        struct uninitialized_copy_n_fn {
+            template <class InIter, class Size, class FwdIter>
+            auto constexpr operator()(InIter first, Size n, FwdIter result) const -> FwdIter
+            {
+                return std::uninitialized_copy_n(first, n, result);
+            }
+        };
+        inline constexpr uninitialized_copy_n_fn uninitialized_copy_n = {};
+
+        struct uninitialized_safe_move_fn {
+            template <class InIter, class FwdIter>
+            auto constexpr operator()(InIter first, InIter last, FwdIter result) const -> FwdIter
+            {
+                if constexpr (core::is_nothrow_move_constructible<core::value_type<InIter>>)
+                {
+                    return std::uninitialized_move(first, last, result);
+                }
+                else
+                {
+                    return std::uninitialized_copy(first, last, result);
+                }
+            }
+        };
+        inline constexpr uninitialized_safe_move_fn uninitialized_safe_move = {};
+
+        struct uninitialized_safe_move_n_fn {
+            template <class FwdIter1, class Size, class FwdIter2>
+            auto constexpr operator()(FwdIter1 first, Size n, FwdIter2 result) const -> FwdIter2
+            {
+                if constexpr (core::is_nothrow_move_constructible<core::value_type<FwdIter1>>)
+                {
+                    return std::uninitialized_move_n(first, n, result);
+                }
+                else
+                {
+                    return std::uninitialized_copy_n(first, n, result);
+                }
+            }
+        };
+        inline constexpr uninitialized_safe_move_n_fn uninitialized_safe_move_n = {};
 
         template <class T>
         class ref {
