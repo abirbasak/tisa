d591f3975fc8e092181c65c02e8a6c8109b1caa0
abirbasak <abirbasak@gmail.com>
2019-03-22 16:21:45 +0530

    adding a few more callbacks

--- source/azuik/io/http.cpp
+++ source/azuik/io/http.cpp
@@ -78,7 +78,58 @@ namespace azuik
                     , m_event_loop{::uv_default_loop()}
                     , m_context{}
                 {}
-                void run() {}
+                void run()
+                {
+                    checked >> ::uv_tcp_init(&m_event_loop, &m_context.handle);
+                    struct sockaddr_in addr;
+                    checked >> ::uv_ip4_addr(m_host, m_port, &addr);
+                    checked >> ::uv_tcp_bind(&m_context.handle, (struct sockaddr*)&addr, 0);
+                    checked >> ::uv_listen(as_stream(&m_context.handle), 128, on_connect);
+                }
+                static void on_connect(::uv_stream_t* stream, int status)
+                {
+                    auto* ctx = new context{};
+                    checked >> ::uv_tcp_init(stream->loop, &ctx->handle);
+                    ctx->handle.data = ctx;
+                    int r = ::uv_accept(stream, as_stream(&ctx->handle));
+                    if (r != 0)
+                    {
+                        auto* shutdown_request = new uv_shutdown_t{};
+                        checked >> ::uv_shutdown(shutdown_request, as_stream(&ctx->handle),
+                                                 on_shutdown);
+                    }
+                    ::http_parser_init(&ctx->parser, HTTP_REQUEST);
+                    ctx->parser.data = ctx;
+                    checked >> ::uv_read_start(as_stream(&ctx->handle), on_alloc, on_read);
+                }
+                static void on_alloc(::uv_handle_t* handle, std::size_t suggested_size,
+                                     ::uv_buf_t* buffer)
+                {}
+                static void on_read(::uv_stream_t* stream, ssize_t nread, ::uv_buf_t const* buffer)
+                {}
+                static void on_shutdown(uv_shutdown_t* shutdown_request, int status)
+                {
+                    (void)status;
+                    ::uv_close(as_handle(shutdown_request->handle), on_close);
+                    delete shutdown_request;
+                }
+                static void on_close(uv_handle_t* handle)
+                {
+                    delete get_context(handle);
+                }
+                static ::uv_handle_t* as_handle(::uv_stream_t* s)
+                {
+                    return reinterpret_cast<::uv_handle_t*>(s);
+                }
+                static ::uv_stream_t* as_stream(::uv_tcp_t* socket)
+                {
+                    return reinterpret_cast<::uv_stream_t*>(socket);
+                }
+                template <class Handle>
+                static context* get_context(Handle* handle)
+                {
+                    return static_cast<context*>(handle->data);
+                }
 
             private:
                 char const* m_host;
