56dd3f3c101f67b71a450f5903e81f23aaeb3db9
abirbasak <abirbasak@gmail.com>
2019-03-27 12:55:29 +0530

    adding svg example

---
+++ examples/svg-example.cpp
@@ -0,0 +1,14 @@
+#include <azuik/io/svg.hpp>
+#include <iostream>
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+    using namespace azuik::io;
+    svg sample1{0, 0, 100, 100};
+    sample1.push_back(svg::circle{.cx = 50.0, .cy = 50.0, .r = 40.0});
+    sample1.push_back(svg::ellipse{50.0, 50.0, 50.0, 20.0});
+    sample1.push_back(svg::line{0.0, 50.0, 100.0, 50.0});
+    sample1.push_back(svg::rect{5.0, 90.0, 90.0, 5.0});
+    sample1.write(std::cout);
+    std::cout << std::endl;
+}
